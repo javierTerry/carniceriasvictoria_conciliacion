ac120ea0244f56289588cd8671ff266cda3281b3
Javier Hernandez <javierv31@gmail.com>
2026-03-16 21:59:02 -0600

    Corrección de Visibilidad de Icono de Estatus, ya aue el icono de cambio solo debe salir en el menu tickets

--- ajax/vtaqry.php
+++ ajax/vtaqry.php
@@ -206,10 +206,12 @@ if ($action == 'ajax') {
                 </button>
               <?php endif; ?>
               
+              <?php if (empty($status_filter)): ?>
               <button type="button" class='btn btn-default btn-xs action-btn-victoria' title='Cambiar Estatus'
                 onclick="changeStatusPrompt('<?php echo $r['mov_id']; ?>', '<?php echo htmlspecialchars($r['cliente'], ENT_QUOTES); ?>', '<?php echo number_format($r['sumimp'], 2); ?>', '<?php echo htmlspecialchars($r['fname'], ENT_QUOTES); ?>', '<?php echo $status; ?>')">
                 <i class="glyphicon glyphicon-retweet"></i>
               </button>
+              <?php endif; ?>
 
               <button type="button" class='btn btn-default btn-xs action-btn-victoria' title='Imprimir Venta'
                 onclick="printTicket('<?php echo $r['id']; ?>', '<?php echo $r['branch_label']; ?>')">
